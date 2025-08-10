de88ea67c3f93e2d8e9a919078d52c839ad8e99c
mehdi arezoomand <mahdi_arezoomand@yahoo.com>
2025-08-10 12:50:53 +0330

    test

---
+++ src/test/java/com/sinatech/lib/domain/rialDigital/RialDigitalServiceTest.java
@@ -0,0 +1,21 @@
+package com.sinatech.lib.domain.rialDigital;
+
+import com.rahand.common.dto.ResponseDto;
+import com.sinatech.lib.domain.rialDigital.dto.RialDigitalGetTokenReponse;
+import com.sinatech.lib.domain.rialDigital.dto.RialDigitalGetTokenRequest;
+import com.sinatech.lib.domain.rialDigital.service.RialDigitalService;
+import com.sinatech.lib.domain.rialDigital.service.spec.RialDigital;
+import org.junit.Test;
+
+public class RialDigitalServiceTest {
+
+    @Test
+    public void getToken(){
+//        RialDigitalGetTokenRequest rialDigitalGetTokenRequest = new RialDigitalGetTokenRequest();
+//        rialDigitalGetTokenRequest.setUserId("9910433274");
+//        ResponseDto<RialDigitalGetTokenReponse> res = new RialDigitalService().getToken(rialDigitalGetTokenRequest);
+//
+//        System.out.println(res.getResponseData().getToken());
+
+    }
+}
